f8551301c1011d02b0245f730cb2c6faa0bff461
sander <sander@pop-os.localdomain>
2021-06-18 08:51:11 -0300

    section 10 - first component

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+    <learning-resources 
+      v-for="res in storedResources" 
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resources>
+  </ul>
+</template>
+
+<script>
+import LearningResources from './components/learning-resources/LearningResource.vue';
+
+export default {
+  components: {
+    LearningResources,
+  },
+  data() {
+    return {
+      storedResources: [
+        { 
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'VueJs Doc',
+          link: 'https://vuejs.org'
+        },
+        { 
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google',
+          link: 'https://google.com'
+        }
+      ]
+    }
+  }
+}
+</script>
