e8030a94b64fb2a2195b2a953cc65a3156c26506
sanderdsz <sanderdsz@gmail.com>
2021-06-22 15:53:49 -0300

    button component

--- prj-cmp-01-starting-setup/src/main.js
+++ prj-cmp-01-starting-setup/src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import BaseCard from './components/user-interface/BaseCard.vue'
+import BaseButton from './components/user-interface/BaseButton.vue'
+
 
 const app = createApp(App);
 
 app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
 
 app.mount('#app');
