fe50cdb3ddc0c415664eba2aa25041db86897041
sanderdsz <sanderdsz@gmail.com>
2021-06-22 21:25:54 -0300

    modal added

--- prj-cmp-01-starting-setup/src/components/learning-resources/AddResources.vue
+++ prj-cmp-01-starting-setup/src/components/learning-resources/AddResources.vue
@@ -1,4 +1,12 @@
 <template>
+  <base-dialog v-if="inputIsInvalid" title="Invalid Input" @close="confirmError">
+    <template #default>
+      <p>Unfortunately, at least one input value is invalid.</p>
+    </template>
+    <template #actions>
+      <base-button @click="confirmError">Ok</base-button>
+    </template>
+  </base-dialog>
   <base-card>
     <form @submit.prevent="submitData">
       <div class="form-control">
@@ -21,15 +29,35 @@
 </template>
 
 <script>
+import BaseButton from "../user-interface/BaseButton.vue"
+
 export default {
+  components: { BaseButton },
   inject: ['addResource'],
+  data() {
+    return {
+      inputIsInvalid: false,
+    }
+  },
   methods: {
     submitData() {
       const enteredTitle = this.$refs.titleInput.value;
       const enteredDescription = this.$refs.descInput.value;
       const enteredLink = this.$refs.linkInput.value;
 
+      if (
+        enteredTitle.trim() === '' ||
+        enteredDescription.trim() === '' ||
+        enteredLink.trim === ''
+      ) {
+        this.inputIsInvalid = true;
+        return
+      }
+
       this.addResource(enteredTitle, enteredDescription, enteredLink);
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
     }
   }
 }
--- prj-cmp-01-starting-setup/src/main.js
+++ prj-cmp-01-starting-setup/src/main.js
@@ -3,11 +3,13 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import BaseCard from './components/user-interface/BaseCard.vue'
 import BaseButton from './components/user-interface/BaseButton.vue'
+import BaseDialog from './components/user-interface/BaseDialog.vue'
 
 
 const app = createApp(App);
 
 app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
+app.component('base-dialog', BaseDialog);
 
 app.mount('#app');
